55a7a08e3ebeeb0541bbc1c4f5977eb5d73ffd12
Bruno Moleta <brunomoleta@pm.me>
2024-05-20 18:03:44 -0300

    :recycle: refactor/make clickable area bigger
    Area where the user may click to
    switch the color theme became larger.

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-const darkModeToggleBtn = document.querySelector(".toggle-btn");
+const darkModeToggle = document.querySelector(".toggle-btn");
+const darkModeToggleBtn = document.querySelector(".toggle");
 let darkMode = localStorage.getItem("darkmode");
 
 const darkModeEnabled = () => {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("darkmode", "enabled");
+  document.body.classList.add("dark-mode");
+  localStorage.setItem("darkmode", "enabled");
 };
 
 const darkModeDisabled = () => {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("darkmode", null);
+  document.body.classList.remove("dark-mode");
+  localStorage.setItem("darkmode", null);
 };
 
 // Check initial dark mode state and apply it
 if (darkMode === "enabled") {
-    darkModeEnabled();
-    darkModeToggleBtn.classList.add("active");
+  darkModeEnabled();
+  darkModeToggle.classList.add("active");
 } else {
-    darkModeDisabled();
-    darkModeToggleBtn.classList.remove("active");
+  darkModeDisabled();
+  darkModeToggle.classList.remove("active");
 }
 
 darkModeToggleBtn.addEventListener("click", () => {
-    // Toggle dark mode
-    if (darkMode !== "enabled") {
-        darkModeEnabled();
-        darkModeToggleBtn.classList.add("active");
-    } else {
-        darkModeDisabled();
-        darkModeToggleBtn.classList.remove("active");
-    }
-    // Update the dark mode state variable
-    darkMode = localStorage.getItem("darkmode");
+  // Toggle dark mode
+  if (darkMode !== "enabled") {
+    darkModeEnabled();
+    darkModeToggle.classList.add("active");
+  } else {
+    darkModeDisabled();
+    darkModeToggle.classList.remove("active");
+  }
+  // Update the dark mode state variable
+  darkMode = localStorage.getItem("darkmode");
 });
